4fa67c6fe98803e41b7c363b5bffdad7bc318252
Jason <jason.beach@mgmt-tech.org>
2018-02-01 16:00:31 -0500

    add /signup route with email check and password encrypt

--- config/routes/routes.js
+++ config/routes/routes.js
@@ -1,5 +1,7 @@
 var passport = require("passport")
 var express = require("express")
+var User = require("../userModel")
+var bcrypt = require("bcryptjs")
 
 
 // middleware to check authentication of url
@@ -10,12 +12,61 @@ var checkAuthentication = function(req, res, next) {
     res.redirect("/login")
 }
 
+var encryptPassword = function(plaintextPass) {
+    var saltRounds = 10
+    return bcrypt.hashSync(plaintextPass, saltRounds);
+}
+
+
+
+
+
 module.exports = function(app) {
 
     var router = express.Router()
 
     router.get("/", function(req, res) {
         res.render("home")
+    })
+
+
+    router.get("/signup", function(req, res) {
+        var message = req.flash("error")[0] //possible errors from signup process
+        console.log(message)
+        if (message) {
+            res.render("signup", { messages: message })
+        } else {
+            res.render("signup")
+        }
+    })
+
+    router.post("/signup", function(req, res) {
+        var password = encryptPassword(req.body.password)
+        var data = {
+            firstName: req.body.firstname,
+            lastName: req.body.lastname,
+            userName: req.body.username,
+            password: password,
+            roles: req.body.role
+        }
+        User.findOne({ userName: req.body.username }, function(err, chkuser) {
+            console.log(chkuser)
+            if (err) {
+                console.log(err)
+            }
+            if (chkuser) {
+                res.render("signup", { messages: "email already exists" })
+            }
+            if (chkuser == null) {
+                User.create(data, function(err, user) {
+                    if (err) {
+                        return next(err)
+                    } else {
+                        return res.redirect("login")
+                    }
+                })
+            }
+        })
     })
 
     router.get("/login", function(req, res) {
